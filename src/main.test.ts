cb83ef277f1af46b7afdd61bc756e1da72bdcaa3
ArmorDarks <git@lavrins.com>
2019-10-05 22:40:57 +0300

    [misc] add testing files

---
+++ src/main.test.ts
@@ -0,0 +1,9 @@
+/* tslint-env jest */
+
+import main from './main'
+
+describe('main', () => {
+  it('should be test', () => {
+    expect(main).toBe('test')
+  })
+})
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import './main.scss'
+
+console.log("'hello2'")
+
+export default 'test'
