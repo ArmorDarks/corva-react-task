86bf2495734af92685d2bd902dfd931f824fdef0
ArmorDarks <git@lavrins.com>
2019-10-06 12:21:52 +0300

    [app] update test file

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -2,10 +2,11 @@
 
 import React from 'react'
 import { shallow } from 'enzyme'
+
 import App from './App'
 
-describe('main', () => {
-  it('should be test', () => {
+describe('App', () => {
+  it('should render', () => {
     expect(shallow(<App />)).toMatchSnapshot()
   })
 })
