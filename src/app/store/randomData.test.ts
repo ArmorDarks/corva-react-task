053340dedfc847eb17482160a9819b190a340a3d
ArmorDarks <git@lavrins.com>
2019-10-06 21:10:37 +0300

    [app] add `randomData` reducer tests

---
+++ src/app/store/randomData.test.ts
@@ -0,0 +1,42 @@
+/* eslint-env jest */
+/* eslint no-unused-vars: "off" */
+/* eslint @typescript-eslint/no-unused-vars: "error" */
+
+import { reducer, initState, createUpdateData, createUpdateThreshold } from './randomData'
+
+describe('randomData reducer', () => {
+  describe('with no action', () => {
+    it('should return same state', () => {
+      const newState = reducer(undefined, {} as any)
+      expect(newState).toEqual(initState)
+    })
+  })
+
+  describe('with UpdateData action', () => {
+    it('should change data', () => {
+      const newData = { value: 5, timestamp: 232323 }
+      const action = createUpdateData(newData)
+
+      const newState = reducer(undefined, action)
+
+      expect(newState).toEqual({
+        ...initState,
+        data: [...(initState.data || []), newData]
+      })
+    })
+  })
+
+  describe('with UpdateThreshold action', () => {
+    it('should change threshold', () => {
+      const newThreshold = 5
+      const action = createUpdateThreshold(newThreshold)
+
+      const newState = reducer(undefined, action)
+
+      expect(newState).toEqual({
+        ...initState,
+        threshold: newThreshold
+      })
+    })
+  })
+})
